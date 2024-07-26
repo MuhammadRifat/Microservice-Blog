5ccf4732cec592b209256b15630d8ce97c6ab410
Rifat <hrifat450@gmail.com>
2024-07-26 12:31:32 +0600

    file service done

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
 import { ImageLibraryModule } from './modules/image-library/image-library.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env'
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
     RabbitmqModule,
     MongooseModule.forRoot(process.env.DB_URL),
