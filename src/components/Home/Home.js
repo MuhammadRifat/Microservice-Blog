3a777feeabf4178919934d15dd041b172a50c1d8
Rifat <hrifat450@gmail.com>
2024-07-31 15:51:22 +0600

    frontend done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -42,26 +42,28 @@ const Home = () => {
     // handle search bar
     const handleSearch = (search) => {
 
-        setBlogs([]);
-        setIsLoading(true);
-        const api = search ? `${API_URL.BLOG}/blog/search?q=${search}` : `${API_URL.BLOG}/blog`;
-        fetch(api, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-            .then(res => res.json())
-            .then(data => {
-                setIsLoading(false);
-                setBlogs(data?.data);
-
+        if (!isLoading && search) {
+            setBlogs([]);
+            setIsLoading(true);
+            const api = search ? `${API_URL.BLOG}/blog/search?q=${search}` : `${API_URL.BLOG}/blog`;
+            fetch(api, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
             })
-            .catch((error) => {
+                .then(res => res.json())
+                .then(data => {
+                    setIsLoading(false);
+                    setBlogs(data?.data);
 
-                setIsLoading(false);
-                console.log(error.message);
-            })
+                })
+                .catch((error) => {
+
+                    setIsLoading(false);
+                    console.log(error.message);
+                })
+        }
     }
 
     // Fix navbar
@@ -93,7 +95,7 @@ const Home = () => {
             <div className='text-right p-8'>
                 <span>Total: <b>{resPagination.totalIndex || 0}</b></span>
                 <span className='mx-2'>Current Page: <b>{resPagination.currentPage || 0}</b></span>
-                <input className='border-2 mx-2 w-16' defaultValue={pagination.limit} onBlur={(e) => setPagination({ ...pagination, limit: e.target.value > 1 ? Number(e.target.value) : 10 })} />
+                <input className='border-2 mx-2 w-16' defaultValue={pagination.limit} onBlur={(e) => setPagination({ ...pagination, limit: (e.target.value > 1 && e.target.value <=100) ? Number(e.target.value) : 10 })} />
 
                 <button className='border-0 mx-2' onClick={() => setPagination({ ...pagination, page: pagination.page > 1 ? pagination.page - 1 : 1 })}>Prev</button>
                 <button className='border-0 mx-2' onClick={() => setPagination({ ...pagination, page: pagination.page + 1 })}>Next</button>
